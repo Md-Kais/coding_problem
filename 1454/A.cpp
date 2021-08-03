3b5990c5e9f2e7e183bc7309e0e0d01cf06fee9f
Md-Kais <mdkais3@gmail.com>
2021-08-03 12:12:37 +0600

    Add solution for 1454A

---
+++ 1454/A.cpp
@@ -0,0 +1,53 @@
+//ALLAH WILL HELP ME
+//nerd!
+#include<bits/stdc++.h>
+using namespace std;
+
+//<--------------------------------------------typo section--------------------------------------------->
+typedef long long ll;
+typedef vector<int> vi;
+#define over return 0;
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+#define test int t;cin>>t;while(t--)
+//<--------------------------------------------------------------------------------------------------->
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+//continue;
+/*
+AKASH vora tara, **** mara SHARA
+*/
+///----------------------------> YOUR DOOM STARTS HERE ------------------------------------>>
+int main(){
+
+    optimize();
+    ///test dekho
+
+    test{
+
+    int n;cin>>n;
+
+    if(n%2==0){for(int i=n;i>=1;i--) { cout<<i<<" ";}}
+    else{for(int i=n;i>floor(n/2)+1;i--) cout<<i<<" ";for(int i=1;i<=floor(n/2)+1;i++) cout<<i<<" ";}
+
+
+    }
+
+
+    //LAST LINE:THIS WORLD is full of cheaters,LIARS! I see them everywhere! their real face lives under their mask or niqab or hijab or their makeup! F Y A !
+over
+}
+//okay! bye!
+//parasite eve!
+//can you feel my heart?
+//doom eternal
+//ibra
