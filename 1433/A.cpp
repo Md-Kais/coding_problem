761e9266fe173dbe1d3b3e3ef0d04297ebdda99c
Md-Kais <mdkais3@gmail.com>
2021-08-03 12:09:47 +0600

    Add solution for 1433A

---
+++ 1433/A.cpp
@@ -0,0 +1,133 @@
+//get fucked nerd!
+#include<bits/stdc++.h>
+using namespace std;
+
+//<--------------------------------------------typo section--------------------------------------------->
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+//bees make honey
+typedef double dl;
+
+//<----------------------------------------------------Narcos----------------------------------------------->
+#define endl "\n"
+#define Y cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define yes cout<<"Yes"<<endl;
+#define no cout<<"No"<<endl;
+#define sp " "
+#define PB push_back
+#define F first
+#define S second
+#define as(a) (a).begin(),(a).end()
+#define ras(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define ki 1LL
+#define MOD 1000000000
+#define over return 0;
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+#define test int t;cin>>t;while(t--)
+
+
+//<-----------------------------------------------------Const--------------------------------------------------->
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+// i make cummy
+const ll infLL = 9000000000000000000;
+//cholen shuru kori!
+
+//<--------------------------------------------------------------------------------------------------->
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+//continue;
+/*
+AKASH vora tara, **** mara SHARA
+*/
+//-----------------------------> YOUR DOOM STARTS HERE ------------------------------------>>
+int main(){
+
+    optimize();
+
+    test{     int x;cin>>x;string h=to_string(x);
+    int y=h[0]-'0';
+    ll ans=10*((y)-1);
+    int base=y;
+    int j=1;
+    for(int i=1;i<=5;i++){
+        if(x>=base){
+            ans+=i;
+            base=base*10+y;
+        }
+        else{
+            break;
+        }
+
+    }
+    cout<<ans<<endl;
+         }
+
+   //ibra abracadabra
+
+
+
+
+   //please remain calm
+
+
+
+
+
+   //the end has arrived
+
+
+
+
+   //Doom eternal
+
+
+
+
+   //bleeding is doesnot matter if person is death
+   //cholen vai bish khai
+
+
+
+
+   //koi jabo jani na
+   //ki likhi egula?
+   //egula pore ki hobe?
+   //portesen ken egula?
+
+
+
+
+  // comment porte hoy na?
+
+
+
+
+
+
+   //vapa pitha khai na
+   //ami bish khai
+   // shobai bish khay
+   //aladdin
+    //LAST LINE:THIS WORLD is full of cheaters,LIARS! I see them everywhere! their real face lives under their mask or niqab or hijab or their makeup! F Y A !
+over
+}
+//okay! bye!
+//parasite eve!
+//can you feel my heart?
+//doom eternal
+//ibra
