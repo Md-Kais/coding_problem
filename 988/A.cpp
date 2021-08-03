56053694d41e8c2fb8a6b95e6352cd99b520caaf
Md-Kais <mdkais3@gmail.com>
2021-08-03 12:12:09 +0600

    Add solution for 988A

---
+++ 988/A.cpp
@@ -0,0 +1,133 @@
+//get fucked nerd!
+#include<bits/stdc++.h>
+using namespace std;
+
+//<--------------------------------------------typo section--------------------------------------------->
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+//bees make honey
+typedef double dl;
+
+//<----------------------------------------------------Narcos----------------------------------------------->
+#define endl "\n"
+#define Y cout<<"YES"<<endl;
+#define NO cout<<"NO"<<endl;
+#define yes cout<<"Yes"<<endl;
+#define no cout<<"No"<<endl;
+#define sp " "
+#define PB push_back
+#define F first
+#define S second
+#define as(a) (a).begin(),(a).end()
+#define ras(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define ki 1LL
+#define MOD 1000000000
+#define over return 0;
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+#define test int t;cin>>t;while(t--)
+
+
+//<-----------------------------------------------------Const--------------------------------------------------->
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+// i make cummy
+const ll infLL = 9000000000000000000;
+//cholen shuru kori!
+
+//<--------------------------------------------------------------------------------------------------->
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu () {            cerr << endl;}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
+
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+//continue;
+/*
+AKASH vora tara, **** mara SHARA
+*/
+//-----------------------------> YOUR DOOM STARTS HERE ------------------------------------>>
+int main(){
+
+    optimize();
+
+
+
+       int n,m;
+       cin>>n>>m;
+       int a[1200]={0};int b[n+1];vector<int>v;
+       for(int i=0;i<n;i++){
+          cin>>b[i];
+          if(a[b[i]]==0){
+               a[b[i]]=1;
+               v.PB(i+1);
+
+          }
+       }
+       if(v.size()>=m){
+            Y
+            for(int i=0;i<m;i++){
+                cout<<v[i]<<" ";
+            }
+            cout<<endl;
+       }
+       else{
+         NO
+       }
+
+
+   //ibra abracadabra
+
+
+
+
+   //please remain calm
+
+
+
+
+
+   //the end has arrived
+
+
+
+
+   //Doom eternal
+
+
+
+
+   //bleeding is doesnot matter if person is death
+   //cholen vai bish khai
+
+
+
+
+   //koi jabo jani na
+   //ki likhi egula?
+   //egula pore ki hobe?
+   //portesen ken egula?
+
+
+
+
+  // comment porte hoy na?
+
+
+
+
+over
+}
+//okay! bye!
+//parasite eve!
+//can you feel my heart?
+//doom eternal
+//ibra
